a3546f4de72a5d6b1bf73ce63f0b9e26735df46e
suresh <sureshpradhana19@gmail.com>
2024-01-14 19:27:11 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import solid from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro-frontend-masters.vercel.app/',
+  integrations: [solid({
+    include: ["**/solid/*"]
+  }), react({
+    include: ["**/react/*"]
+  })],
+  output: "hybrid",
+  adapter: vercel()
+});
