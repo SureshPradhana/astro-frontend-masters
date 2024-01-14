bbea083bca32045aa69856f784a7973999c0befe
suresh <sureshpradhana19@gmail.com>
2024-01-14 19:30:22 +0530

    site fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-frontend-masters.vercel.app/',
+  site: 'https://astro-frontend-masters-2kw2.vercel.app/',
   integrations: [solid({
     include: ["**/solid/*"]
   }), react({
